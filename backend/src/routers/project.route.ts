762faefa8b2b55d9d7c88ab416b044197dbcf207
Xhechar <115901405+Xhechar@users.noreply.github.com>
2024-06-08 17:25:13 +0300

    upto date

--- backend/src/routers/project.route.ts
+++ backend/src/routers/project.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addNewProject, fetchAllCreatedProjects, removeProject, updateExistingProject } from "../controllers/product.controller";
+import { addNewProject, fetchAllCreatedProjects, fetchProjectByID, removeProject, updateExistingProject } from "../controllers/product.controller";
 
 export let my_route = Router();
 
@@ -7,4 +7,5 @@ export let my_route = Router();
 my_route.post('/new-project', addNewProject);
 my_route.get('/all-projects', fetchAllCreatedProjects);
 my_route.put('/update-project/:project_id', updateExistingProject);
-my_route.delete('/delete-project/:project_id', removeProject)
+my_route.delete('/delete-project/:project_id', removeProject);
+my_route.get('/one-project/:project_id', fetchProjectByID);
